d3167175e883b64c3d4bfa8732f3896b09868dca
unknown <manoranjanjenamitsy17@gmail.com>
2025-01-31 18:56:20 +0530

    Working on this branch

---
+++ photogram/src/context/userAuthContext.tsx
@@ -0,0 +1,15 @@
+// import { User } from 'firebase/auth';
+// import {createContext} from 'react';
+
+
+// type AuthContextData={
+//     user:User|null;
+//     logIn: typeof login;
+//     signUp:typeof signup;
+//     logout: typeof LogOut;
+
+// }
+
+// export const userAuthContext=createContext<AuthContextData>({
+    
+// })
--- photogram/src/routes.tsx
+++ photogram/src/routes.tsx
@@ -6,42 +6,44 @@ import CreatePost from "./pages/post";
 import Profile from "./pages/profile";
 import MyPhotos from "./pages/myphotos";
 import Signup from "./pages/signup";
+import ProtectedRoutes from "./components/ui/ProtectedRoutes";
 
 export const router = createBrowserRouter([
-
-
     {
-        path:"/home",
-        element:<Home/>,
-        errorElement:<Error/>
+        element: <ProtectedRoutes />,
+        children: [  // Corrected syntax here
+            {
+                path: "/",
+                element: <Home />,
+                errorElement: <Error />
+            },
+            {
+                path: "/post",
+                element: <CreatePost />,
+                errorElement: <Error />
+            },
+            {
+                path: "/profile",
+                element: <Profile />,
+                errorElement: <Error />
+            },
+            {
+                path: "/myphotos",
+                element: <MyPhotos />,
+                errorElement: <Error />
+            },
+        ]
     },
     {
-        path:"/post",
-        element:<CreatePost/>,
-        errorElement:<Error/>
-    },
-
-    {
-        path:"/profile",
-        element:<Profile/>,
-        errorElement:<Error/>
+        path: "/login",
+        element: <Login />,
+        errorElement: <Error />
     },
     {
-        path:"/myphotos",
-        element:<MyPhotos/>,
-        errorElement:<Error/>
-    },
-    {
-        path:"/login",
-        element:<Login/>,
-        errorElement:<Error/>
-    },
-    {
-        path:"/signup",
-        element:<Signup/>,
-        errorElement:<Error/>
+        path: "/signup",
+        element: <Signup />,
+        errorElement: <Error />
     }
-])
-
+]);
 
 export default router;
